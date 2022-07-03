1fbf1b38a4982ac2cbcd1f31474cf36cb8d3afc4
Shadow-Merry <merryrock6@gmail.com>
2022-07-03 12:02:38 -0700

    multiple returns

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,8 @@
 def print_matrix_integer(matrix=[[]]):
     for x in matrix:
         for y in x:
-            print("{:d}".format(y), end=" ")
-        print()
+            if y == (len(x) - 1):
+                end = ""
+            else:
+                end = " "
+            print("{:d}".format(y), end = end)
